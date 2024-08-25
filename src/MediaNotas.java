8557e85338ff6ff883719dde396764a3d788f292
Alef <alefsilvalp@gmail.com>
2024-08-25 20:52:20 -0300

    acrescentando o calculo da media em um Método

--- src/MediaNotas.java
+++ src/MediaNotas.java
@@ -5,28 +5,33 @@ public class MediaNotas {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
-        // Variáveis para armazenar as notas
-        double nota1, nota2, nota3, nota4;
-
         // Leitura das notas
         System.out.print("Digite a primeira nota: ");
-        nota1 = scanner.nextDouble();
+        double nota1 = scanner.nextDouble();
 
         System.out.print("Digite a segunda nota: ");
-        nota2 = scanner.nextDouble();
+        double nota2 = scanner.nextDouble();
 
         System.out.print("Digite a terceira nota: ");
-        nota3 = scanner.nextDouble();
+        double nota3 = scanner.nextDouble();
 
         System.out.print("Digite a quarta nota: ");
-        nota4 = scanner.nextDouble();
+        double nota4 = scanner.nextDouble();
 
-        // Cálculo da média
-        double media = (nota1 + nota2 + nota3 + nota4) / 4;
 
-        // Impressão da média
-        System.out.println("A média das notas é: " + media);
+        CalculoMedia(nota1, nota2, nota3, nota4);
         
         scanner.close();
     }
+
+    public  static void CalculoMedia(double nota1, double nota2, double nota3, double nota4) {
+          // Cálculo da média
+        
+        double soma = nota1 + nota2 + nota3 + nota4;
+        double media = soma / 4;
+
+          // Impressão da média
+        System.out.println("A média das notas é: " + media);
+    }
+    
 }
